22d1b83f06af58817a67937a78b4668c64d411ba
JAISHNAVPRASAD-DarklIous <rbinternationalnetwork@gmail.com>
2020-10-09 10:52:04 +0530

    RMX1831: sakura bringup

--- lineage_RMX1831.mk
+++ lineage_RMX1831.mk
@@ -11,6 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from RMX1831 device
 $(call inherit-product, device/realme/RMX1831/device.mk)
 
+# Bootanimation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Lawnchair
+LAWNCHAIR_OPTOUT=true
+
+# ProjectSakura
+SAKURA_GAPPS := false
+# SAKURA_OFFICIAL := true
+
+# Build broken rules
+BUILD_BROKEN_DUP_RULES := true
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1831
 PRODUCT_MANUFACTURER := realme
