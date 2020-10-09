711298cc8a5a69219b11f6133babd2db6718137b
eldraco19 <ayush@disroot.org>
2020-10-09 11:03:57 +0530

    RMX1831: add Gcam GO

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# GCamGo
+PRODUCT_PACKAGES += \
+    GCam
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
