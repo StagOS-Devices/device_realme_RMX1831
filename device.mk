37ee76e6752e88010cf5d1cc128437d180aaf5fa
Ayush <ayush@disroot.org>
2020-12-01 10:47:19 +0530

    RMX1831: add Ims plugin xml
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>
    Signed-off-by: Ayush <ayush@disroot.org>

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
     $(LOCAL_PATH)/permissions/com.mediatek.op.ims.common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.mediatek.op.ims.common.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
+    $(LOCAL_PATH)/permissions/com.mediatek.ims.plugin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.mediatek.ims.plugin.xml
+
+
 
 # Lights
 PRODUCT_PACKAGES += \
